651d4564fef0ce60d0b65955d6e4bf2708fd63db
prabhakar025 <prabhakar263801@gmail.com>
2025-07-24 11:56:44 +0530

    code

---
+++ MyClass.java
@@ -0,0 +1,14 @@
+public class MyClass {
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(5, 10));       // int version
+        System.out.println(add(5.5, 6.5));    // double version
+    }
+}
